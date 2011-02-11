d357d6c6a12579d514d169491f4b47026d9cd663
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-11 08:15:22 +0000

    Make this 2011 specific

--- tarball.sh
+++ tarball.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-TARGET=$1
-
-if [ -z $TARGET ]; then
-    echo "Usage: $0 [2011|corporate2]"
-    exit
-fi
-
 svn export . /tmp/pardus
 cd /tmp
-mv pardus/pardus.script{.$TARGET,}
-tar cvjf plymouth-theme-pardus-$TARGET.tar.bz2 pardus
+tar cvjf plymouth-theme-pardus-2011.tar.bz2 pardus
 rm -rf pardus
 
