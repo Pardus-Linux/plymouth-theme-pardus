0ce14b7df88fd86c244d4f779379c047c1861d2f
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-11 08:16:13 +0000

    Add corporate2 branch

---
+++ tarball.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TARGET=$1
+
+if [ -z $TARGET ]; then
+    echo "Usage: $0 [2011|corporate2]"
+    exit
+fi
+
+svn export . /tmp/pardus
+cd /tmp
+mv pardus/pardus.script{.$TARGET,}
+tar cvjf plymouth-theme-pardus-$TARGET.tar.bz2 pardus
+rm -rf pardus
+
